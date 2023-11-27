4572edf70674b2546ea0be14008872aa3e409eef
Sally Lin <LS0723000@gmail.com>
2023-11-28 03:27:19 +0800

    API get environment record limit amount

--- api/Repository/EnvironmentCRUD.py
+++ api/Repository/EnvironmentCRUD.py
@@ -1,32 +1,33 @@
 from Model.Environment import Environment
-from database import db, execute_stmt_in_tran
 from Schema.environment import BaseEnvironmentRecord, GetEnviromentRecord
+from database import db, execute_stmt_in_tran
+
 
 async def get_environment_record() -> list[GetEnviromentRecord]:
-  """Get all environment records actions with db"""
-  stmt = Environment.select()
-  return await db.fetch_all(stmt)
+    """Get all environment records actions with db"""
+    stmt = Environment.select().limit(10).order_by(Environment.c.env_record_id.desc())
+    return await db.fetch_all(stmt)
 
 
 async def get_specific_environment_record(env_record_id: int) -> GetEnviromentRecord:
-  """Get specific environment record actions with db"""
-  stmt = Environment.select().where(Environment.c.env_record_id == env_record_id)
-  return await db.fetch_one(stmt)
+    """Get specific environment record actions with db"""
+    stmt = Environment.select().where(Environment.c.env_record_id == env_record_id)
+    return await db.fetch_one(stmt)
 
 
-async def create_environment_record(env_record: BaseEnvironmentRecord) -> None:
-  """Create new environment record actions with db"""
-  stmt = Environment.insert().values(temperature=env_record.temperature,
-                                     humidity=env_record.humidity,
-                                     pressure=env_record.pressure,
-                                     vibration=env_record.vibration,
-                                     chemical_concentration=env_record.chemical_concentration,
-                                     noise=env_record.noise,
-                                     record_time=env_record.record_time)
-  return await execute_stmt_in_tran([stmt])
+async def create_environment_record(env_record: BaseEnvironmentRecord) -> bool:
+    """Create new environment record actions with db"""
+    stmt = Environment.insert().values(temperature=env_record.temperature,
+                                       humidity=env_record.humidity,
+                                       pressure=env_record.pressure,
+                                       vibration=env_record.vibration,
+                                       chemical_concentration=env_record.chemical_concentration,
+                                       noise=env_record.noise,
+                                       record_time=env_record.record_time)
+    return await execute_stmt_in_tran([stmt])
 
 
-async def delete_specific_environment_record(env_record_id: int) -> None:
-  """Delete specific environment record actions with db"""
-  stmt = Environment.delete().where(Environment.c.env_record_id == env_record_id)
-  return await execute_stmt_in_tran([stmt])
+async def delete_specific_environment_record(env_record_id: int) -> bool:
+    """Delete specific environment record actions with db"""
+    stmt = Environment.delete().where(Environment.c.env_record_id == env_record_id)
+    return await execute_stmt_in_tran([stmt])
--- api/Router/environment.py
+++ api/Router/environment.py
@@ -1,59 +1,65 @@
-from fastapi import APIRouter, Depends
 from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
 from Authentication.JWTtoken import get_current_user
 from Repository.CommonCRUD import check_env_record_values
-from Repository.EnvironmentCRUD import get_environment_record, get_specific_environment_record, create_environment_record, delete_specific_environment_record
-from exception import action_forbidden, no_such_record, bad_request
-
-from Schema.user import GetUser
+from Repository.EnvironmentCRUD import get_environment_record, get_specific_environment_record, \
+  create_environment_record, delete_specific_environment_record
 from Schema.environment import BaseEnvironmentRecord, GetEnviromentRecord
+from Schema.user import GetUser
+from exception import action_forbidden, no_such_record, bad_request
 
 router = APIRouter(prefix="/env", tags=["Environment"])
 
+
 @router.get("/")
 async def get_env_record(current_user: Annotated[GetUser, Depends(get_current_user)]) -> list[GetEnviromentRecord]:
-  """The endpoint of getting all environment records"""
-  if not current_user.is_admin:
-    raise action_forbidden
-  
-  return await get_environment_record()
+    """The endpoint of getting all environment records"""
+    if not current_user.is_admin:
+        raise action_forbidden
+
+    return await get_environment_record()
 
 
 @router.get("/{env_record_id}")
-async def get_spec_env_record(env_record_id: int, current_user: Annotated[GetUser, Depends(get_current_user)]) -> GetEnviromentRecord:
-  """The endpoing of getting specific environment record"""
-  if not current_user.is_admin:
-    raise action_forbidden
-  
-  record = await get_specific_environment_record(env_record_id)
+async def get_spec_env_record(env_record_id: int,
+                              current_user: Annotated[GetUser, Depends(get_current_user)]) -> GetEnviromentRecord:
+    """The endpoint of getting specific environment record"""
+    if not current_user.is_admin:
+        raise action_forbidden
 
-  if not record:
-    raise no_such_record
-  
-  return record
+    record = await get_specific_environment_record(env_record_id)
+
+    if not record:
+        raise no_such_record
+
+    return record
 
 
 @router.post("/")
-async def create_env_record(env_record: BaseEnvironmentRecord, current_user: Annotated[GetUser, Depends(get_current_user)]) -> None:
-  """The endpoint of making new environment record"""
-  if not current_user.is_admin:
-    raise action_forbidden
-  
-  if not await check_env_record_values(env_record):
-    raise bad_request
-  
-  if not await create_environment_record(env_record):
-    raise bad_request
-  
+async def create_env_record(env_record: BaseEnvironmentRecord,
+                            current_user: Annotated[GetUser, Depends(get_current_user)]) -> None:
+    """The endpoint of making new environment record"""
+    if not current_user.is_admin:
+        raise action_forbidden
+
+    if not await check_env_record_values(env_record):
+        raise bad_request
+
+    if not await create_environment_record(env_record):
+        raise bad_request
+
 
 @router.delete("/{env_record_id}")
-async def delete_spec_env_record(env_record_id: int, current_user: Annotated[GetUser, Depends(get_current_user)]) -> None:
-  """The endpoint of deleting specific environment record"""
-  if not current_user.is_admin:
-    raise action_forbidden
+async def delete_spec_env_record(env_record_id: int,
+                                 current_user: Annotated[GetUser, Depends(get_current_user)]) -> None:
+    """The endpoint of deleting specific environment record"""
+    if not current_user.is_admin:
+        raise action_forbidden
 
-  if not await get_specific_environment_record(env_record_id):
-    raise no_such_record
-  
-  if not await delete_specific_environment_record(env_record_id):
-    raise bad_request
+    if not await get_specific_environment_record(env_record_id):
+        raise no_such_record
+
+    if not await delete_specific_environment_record(env_record_id):
+        raise bad_request
